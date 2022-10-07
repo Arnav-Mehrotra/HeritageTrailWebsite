2b7be99146d704e736496aa1d440940930dd1b7e
Arnav Mehrotra <arnavmehrotra.2@gmail.com>
2022-10-07 16:17:35 -0400

    Created basic page for Home, still needs a lot of tweaking
    need to center images, and fix formatting, this was mostly just moving the information

---
+++ src/routes/Home/+page.svelte
@@ -0,0 +1,187 @@
+<script>
+</script>
+
+<div class="text-TextPrimary">
+	<div class="flex flex-row">
+		<div class="basis-1/4" />
+		<div class="basis-2/4 items-center">
+			<p class="tracking-wide leading-7 text-shadow">
+				The 10 km long Upper Canada Heritage Trail in Niagara-on-the-Lake is located on an abandoned
+				railroad right of way that goes south from the Old Town beginning near King and John Streets
+				and runs parallel to Concession 1 Road to end at York Road, between the villages of
+				Queenston and St Davids. Here, the Trail continues as a sub-trail of the Bruce Trail.
+			</p>
+			<p class="tracking-wide leading-7 text-shadow">
+				The Heritage Trail encompasses a variety of landscapes from urban and Carolinian forest to
+				rural orchards and vineyards. It lies entirely within Ontario's Greenbelt. From East and
+				West Line it passes through specialty crop areas before climbing through the Niagara
+				Escarpment Region to York Road. It was formerly a side trail of the Bruce Trail.
+				<a href="https://youtu.be/soGFpsy48KU"
+					>Watch a video of Phase 2 of the Heritage Trail (video courtesy of Rene Bertschi, Skyview
+					Arts Inc.)</a
+				>
+			</p>
+			<img
+				src="Images/Trail July 2021.jpg"
+				alt="The trail as seen on July 2021"
+				class="w-full rounded-t"
+				width="55%"
+			/>
+			<p class="tracking-wide leading-7 text-shadow">
+				A total of 50 trees were planted on the Trail on October 13, 2021. Most of the trees were
+				Chautauqua Oak trees that had been grown at Niagara College from acorns from the Chautauqua
+				neighbourhood. The trees were provided by Canopy Growth Corporation and planted by students
+				from Niagara College.
+			</p>
+
+			<img
+				src="Images/Canopy Growth Tree planting -Oct 13.jpg"
+				alt="Shown is Sean Webster, Head, Government and Stakeholder Relations, Canada at Canopy Growth Corporation."
+				class="rounded-t"
+				width="40%"
+			/>
+			<hr class="my-5" />
+			<h3 class="text-xl font-semibold">
+				The 4 Phases of the rehabilitation plan by the Town of Niagara-on-the-Lake, under the
+				guidance of the Town's Heritage Trail Committee.
+			</h3>
+			<img
+				src="Images/HeritageTrail - Phases update Sept 2021 - reduced.jpg"
+				alt="Map of Heritage Trial showing the different phases progress, spanning over 10 km."
+				class="rounded-t"
+				width="55%"
+			/>
+			<p class="tracking-wide leading-7 text-shadow">
+				After 2 years of fundraising and trail planning, the Heritage Trail Committee is very
+				pleased to announce the rejuvenation of the whole of Phase One of the trail. 1500 metres
+				have been resurfaced and completely rebuilt for multi-purpose use. Please come and see for
+				yourself!
+			</p>
+			<p class="tracking-wide leading-7 text-shadow">
+				HERITAGE TRAIL REHABILITATION AND IMPROVEMENTS ARE DESIGNED TO PROCEED IN 4 PHASES. THE
+				THIRD PHASE IS FROM LINE 3 TO LINE 9. THE LAST (4TH) PHASE IS FROM LINE 9 TO YORK ROAD.
+			</p>
+
+			<p class="tracking-wide leading-7 text-shadow">
+				The Heritage Trail Committee has installed 4 large signs on this section clearly outlining
+				the full trail and its historic background. These attractive and dominant signs have been
+				well placed to clearly mark the importance and history of the old rail line and trail. The
+				Committee has enhanced the entrances of the trail at John Street, Paffard and Charlotte
+				Streets with extra plantings and armour stone.
+			</p>
+
+			<p class="tracking-wide leading-7 text-shadow">
+				A fundraising effort was launched in 2019 which has successfully raised over $100,000 for
+				the rehabilitation of the trail. Most of these donations have come from local residents and
+				businesses who have purchased 1 metre of trail for $100. More than 100 people and families
+				have become “Trail Blazers”. As well there have been significant corporate donations - see
+				below for details.
+			</p>
+
+			<div>
+				<h2>Improvements Needed for the Heritage Trail</h2>
+				<p>
+					The Heritage Trail is the only unimproved multi-use trail in the Town of
+					Niagara-on-the-Lake’s Official Plan. It dissects the epicentre of the Wine Route in NOTL,
+					a key destination for cycling, eco-tourism and agri-tourism.As the automobile was
+					responsible for the decline of the railway, it seems fitting that a shift from the
+					automobile to a renewed priority on active transportation has the potential to breathe new
+					life back into the Heritage Trail.
+				</p>
+
+				<p>
+					The 66-foot-wide corridor is visibly broken and even unknown to many residents of
+					Niagara-on-the-Lake. Utilization of the trail has been compromised in the last few years
+					by erosion and washout between Line 9 and York Road, and in other sections.
+				</p>
+
+				<p>
+					The overall integrity of the corridor is eroding due to an inability for people to use it.
+					Vegetation and grass are growing where people used to walk, run, cycle and ride horses.
+					The trail is losing its “visibility” in both a physical sense and in the overall
+					consciousness of the Town of Niagara-on-the-Lake.
+				</p>
+
+				<p>
+					The NOTL Canada Sesquicentennial Committee, created in 2017 to support and execute events
+					to commemorate Canada's 150 years of Confederation, chose the rehabilitation of the
+					Heritage Trail as its Legacy project, to connect communities and to preserve a piece of
+					Canadian history for new generations. A committee of the Town of Niagara-on-the-Lake, The
+					Heritage Trail, was initiated in 2019 and will be directed by a group of volunteers from
+					the community.
+				</p>
+			</div>
+
+			<div>
+				<h3>Heritage Trail Committee Plans</h3>
+				<ul>
+					<li>
+						Install new signage and trail markers to heighten awareness and usability of the Trail.
+					</li>
+					<li>Clean-up and removal of vegetation overgrowth.</li>
+					<li>Repair and sustained maintenance of eroded and impassable areas.</li>
+					<li>Community engagement.</li>
+					<li>Sponsorship opportunities - adopt a section of the Trail!</li>
+				</ul>
+			</div>
+			<hr />
+			<h3>
+				<a href="https://youtu.be/UGNFaZpSkvk"
+					>Click to view video of the rehabilitated section of the trail.</a
+				>
+				PRODUCED IN AUGUST 2020 BY RENE BERTSCHI (Skyview Aerial Video & Photography)
+			</h3>
+
+			<div>
+				<h3>Donate to the Trail!</h3>
+				<p>
+					YOU CAN NOW DONATE TO THE HERITAGE TRAIL ONLINE WITH YOUR VISA OR MASTERCARD CREDIT CARD.
+					THE SERVICE IS SAFE AND SECURE, AND IS MANAGED BY THE TOWN OF NIAGARA-ON-THE-LAKE. <br />
+					TO MAKE A DONATION, <a href="https://ipn.paymentus.com/rotp/TNOL"> GO TO THIS PAGE</a> AND
+					THEN SELECT DONATIONS - HERITAGE TRAIL UNDER "SELECT A PAYMENT TYPE".<br />
+					IF YOU WOULD LIKE TO DONATE IN SOMEONE ELSE'S NAME OR IN MEMORY, PLEASE MAIL A CHEQUE ALONG
+					WITH A NOTE TELLING US THE NAME OF THAT PERSON TO: <br />
+					THE TOWN OF NIAGARA-ON-THE-LAKE, 1593 Four Mile Creek Road, PO Box 100, Virgil, ON L0S 1T0
+					<br />
+					ONCE PAYMENT IS PROCESSED YOU WILL RECEIVE A CHARITABLE TAX RECEIPT BY EMAIL FROM THE TOWN.
+				</p>
+				<br />
+				<h3>Photo of several Trail Blazers who donated to the Trail on July 29, 2019</h3>
+				<img
+					src="Images/July 29 - donors 1-small.jpg"
+					alt="Plaque of several Trail Blazers who donated to the Trail on July 29, 2019"
+					width="45%"
+				/>
+				<h4>
+					<a href="http://www.heritagetrail.ca/resources/HERITAGE%20TRAIL%20-%20Trail%20Blazers.pdf"
+						>DOWNLOAD THE HERITAGE TRAIL COMMITTEE BROCHURE</a
+					>
+				</h4>
+				<p>
+					<a
+						href="http://www.heritagetrail.ca/resources/Upper%20Canada%20Heritage%20Trail%20-%2005-22.pdf"
+						>Download The Heritage Trail Community Brochure</a
+					>
+					<br />
+					<a href="https://youtu.be/OIcak8atu_g"
+						>VIDEO (3 MINUTES): HERITAGE TRAIL PHASE 1, JOHN ST. TO EAST & WEST LINE</a
+					>
+					<br />
+					<a href="https://youtu.be/uaa1VC-IPr0"
+						>VIDEO (5 MINUTES): HERITAGE TRAIL PHASE 4, LINE 9 TO YORK ROAD
+					</a>
+					<br />
+				</p>
+			</div>
+		</div>
+		<div class="basis-1/4" />
+	</div>
+</div>
+
+<svelte:head>
+	<style>
+		.text-shadow {
+			text-shadow: 0px 0px 10px black !important;
+		}
+	</style>
+</svelte:head>
